7ec9c0b90b4cae3d76445543cb64d5e636ffba5e
SebastianRonce <jronceria@ucundinamarca.edu.co>
2024-09-27 17:29:01 -0500

    Intento_1

--- App_Plantify.py
+++ App_Plantify.py
@@ -1,3 +1,4 @@
+from Base_de_Datos import plantas
 class planta:
     def __init__(self, codigo: int, nombre: str, temperatura: str, riego: str, tiempo_riego: str, altura: str, recomendacion: str):
         self.codigo = codigo
@@ -36,14 +37,7 @@ def buscar_por_codigo(codigo: int):
     print("La planta no se encuentra en la base de datos ")
 
 
-plantas.append(planta(1, "Sabila", "20° a 25°C.", "Una cada 10 o 14 dias.", "Durante 3 minutos.", "20 a 25 Cm.", """ La sabila necesita suelos ricos en materia orgánica
-y con buen drenaje que eviten los encharcamientos. """))
 
-plantas.append(planta(2, "Cilantro", "10° a 24°C.", "2 o 3 veces por semana.", "Durante 3 minutos.", "40 a 70 Cm.", """ El cilantro necesita nitrógeno, fósforo y potasio para crecer de manera óptima.
-Además de los nutrientes básicos, también requiere calcio, magnesio y azufre."""))
-
-plantas.append(planta(3, "Calathea", "18° a 24°C.", "1 o 2 veces por semnana.", "Metodo de inversión por 15 minutos.", "12 Cm.", """Las Calathea necesita un suelo húmedo, con buena luz pero no directa al sol
- y buena tierra con mezclas vegetales. """))
 
 def validar_numero(mensaje):
     cantidad = input(mensaje)
